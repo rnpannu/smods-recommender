803d8e11b2f76064c5acae83437ebd814961d7d4
Raj (Raj) Pannu <panr4210@362amalthea>
2026-03-24 14:49:29 -0600

    Added file tracking to changes

--- src/parse_logs.py
+++ src/parse_logs.py
@@ -2,6 +2,7 @@ import csv
 import os
 import subprocess
 import json
+import re
 
 hash: str
 author: str
@@ -10,16 +11,8 @@ date: str
 commits = {}
 
 target_dir = "../../smods"
-
 script_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "commit_csv.sh")
-
-result = subprocess.run(
-    [script_path],  
-    cwd=target_dir,
-    capture_output=True,
-    text=True,
-    check=True
-)
+result = subprocess.run([script_path], cwd=target_dir, capture_output=True, text=True, check=True)
 
 if result.returncode != 0:
     os.abort(1)
@@ -33,20 +26,31 @@ else:
         date = stuff[3]
         diff = subprocess.run(['git', 'show', hash], cwd=target_dir, capture_output=True, text=True, check=True)
         lines_changed = []
+
+        current_file = None
+        file_changes = {str : list}
         for row in diff.stdout.split("\n"):
-            if row[0:2] == "@@":
-                header = row[0:23].strip(" ")
+
+            file_match = re.match(r'^\+\+\+ b/(.*)', row)
+
+            if file_match:
+                current_file = file_match.group(1)
+                print(current_file)
+                if current_file not in file_changes:
+                    file_changes[current_file] = []
+
+
+            match = re.match(r'^@@ -(\d+),?(\d+)? \+(\d+),?(\d+)? @@', row)
+            if match and current_file:
+                new_start = int(match.group(3))
+                new_amt = int(match.group(4)) if match.group(4) else 1
+
+                file_changes[current_file].append([new_start, new_amt])
                 
-                header_items = header.split(" ")[1:3]
-                old = header_items[0].strip("-").split(",")
-                new = header_items[1].strip("+").split(",")
-                
-                if (len(old) != 1 and len(new[0]) != 1):
-                    lines_changed.append(new)
-        commits[hash] = [author, date, email, lines_changed]
+        commits[hash] = [author, date, email, file_changes]
         print("Commit: " + hash, end = ". ")
         print(commits[hash])
-
+        ### Stop at first commit
         break
 
 try:
@@ -62,16 +66,14 @@ except subprocess.CalledProcessError as e:
 
 funcs_dict = json.loads(funcs_json.stdout)
 
-# print(funcs_dict["definitions"])
+### TEST INSERTS
+commits['test'] = ['Goku', 'Sun, 22 Mar 1733 22:11:35 -0300', 'goku@gmail.com', {'demo.lua': [['11', '4']]}]
 
 expertise_map = {}
-
-commits['test'] = ['Goku', 'Sun, 22 Mar 1733 22:11:35 -0300', 'goku@gmail.com', [['11', '4']]]
-
-for func in funcs_dict["definitions"]:
-    func_name = func["name"]
-    start_line = int(func["line_start"])
-    end_line = int(func["line_end"])
+for func in funcs_dict['definitions']:
+    func_name = func['name']
+    func_start_line = int(func['line_start'])
+    func_end_line = int(func['line_end'])
 
     if func_name not in expertise_map:
         expertise_map[func_name] = {}
@@ -79,16 +81,21 @@ for func in funcs_dict["definitions"]:
     for hash, data in commits.items():
         email = data[2]
         date = data[1] 
-        lines_changed = data[3] 
+        files_changed = data[3] 
+        
+        for file, changes in files_changed.items():
+            #change_file = files_changed['demo.lua']
+            func_file = 'game_object.lua'
+            if file == func_file:
+                for change in changes:
+                    change_start = int(change[0])
+                    change_end = change_start + int(change[1])
 
-        for change in lines_changed:
-            change_start = int(change[0])
-
-            if (change_start >= start_line and change_start <= end_line):
-                if email not in expertise_map[func_name]:
-                    expertise_map[func_name][email] = 1
-                else:
-                    expertise_map[func_name][email] += 1
+                    if not (change_end < func_start_line or change_start > func_end_line):
+                        if email not in expertise_map[func_name]:
+                            expertise_map[func_name][email] = 1
+                        else:
+                            expertise_map[func_name][email] += 1
 
 print(expertise_map)
 
